bc373e35ff7413003f761c12fe773ebd638ceb2a
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2025-02-03 16:50:34 -0500

    Another increment

--- src/com/github/multitool/Multitool.java
+++ src/com/github/multitool/Multitool.java
@@ -1,10 +1,16 @@
 package com.github.multitool;
 
+import java.io.File;
 import java.nio.file.FileSystems;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Multitool {
   public static void main(String[] args) {
-    String multitool = getNameAndVersion();
+    processMultitool();
+    processKolMafia();
+    // processJava();
     try {
       startSecondJVM();
     } catch (Exception e) {
@@ -23,9 +29,38 @@ public class Multitool {
     System.out.println(command);
     Runtime.getRuntime().exec(command);
   }
-  private static String getNameAndVersion() {
-    String retVal = "";
-    retVal = StaticEntityLite.getVersion();
+
+  private static void processMultitool() {
+    List<String> tools = processDirectory("Multitool");
+    System.out.println(tools);
+    System.out.println("End Multitool");
+  }
+
+  private static void processKolMafia() {
+    List<String> tools = processDirectory("KoLmafia");
+    System.out.println(tools);
+    System.out.println("End KoLmafia");
+  }
+
+  private static List<String> processDirectory(String nameRoot) {
+    // Returns a list of file names in the current directory that match
+    List<String> retVal = new ArrayList<>();
+    String lcRoot = nameRoot.toLowerCase();
+    String currentWorkingDir = Paths.get("").toAbsolutePath().toString();
+    try {
+      File f = new File(currentWorkingDir);
+      String[] files = f.list();
+      if (files != null) {
+        for (String file : files) {
+          String check = file.toLowerCase();
+          if ((check.startsWith(lcRoot)) && (check.endsWith(".jar"))) {
+            retVal.add(file);
+          }
+        }
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
     return retVal;
   }
 }
