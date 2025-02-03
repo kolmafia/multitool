ee9a6d9d8593f82b52c0b43732e649d2e8ebf043
Jamie Adams <82782908+jaadams5@users.noreply.github.com>
2025-02-03 12:19:38 -0500

    Setting things up

---
+++ Multitool.java
@@ -0,0 +1,22 @@
+import java.nio.file.FileSystems;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            startSecondJVM();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.exit(0);
+    }
+    public static void startSecondJVM() throws Exception {
+        String separator = FileSystems.getDefault().getSeparator();
+        String path = System.getProperty("java.home")
+                + separator + "bin" + separator + "java";
+        String dq = "\"";
+        String jar = "C:/Users/frono/IdeaProjects/MultiTool/out/production/MultiTool/KolMafia-28320.jar";
+        String command = dq + path + dq + " -jar " + jar;
+        System.out.println(command);
+        Runtime.getRuntime().exec(command);
+    }
+}
