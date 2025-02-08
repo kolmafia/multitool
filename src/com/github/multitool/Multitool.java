b649173dba54bc196cdd9ea9cd8bbbcb91643f9e
Jamie Adams <82782908+jaadams5@users.noreply.github.com>
2025-02-08 11:16:40 -0500

    One small step... (#10)
    * Interim park
    
    * park before rearrange
    
    * Park and Ride
    
    * Behaving.

--- src/com/github/multitool/Multitool.java
+++ src/com/github/multitool/Multitool.java
@@ -11,16 +11,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Multitool {
-  public static void main(String[] args) throws IOException {
-    processMultitool();
-    processKolMafia();
-    processJava();
+
+  private static String cdw;
+  private static String localJava;
+  private static int localJavaVersion;
+  private static int preferredJava;
+
+  public static void main(String[] args) {
+    processLocalInformation();
+    displayLocalInformation();
+    preferredJava = getPreferredJava();
+    System.out.println("Preferred Java version: " + preferredJava);
+    ToolData multiData = processTool("multitool");
+    displayToolInformation(multiData);
+    ToolData mafiaData = processTool("kolmafia");
+    displayToolInformation(mafiaData);
     try {
       startSecondJVM();
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
     System.exit(0);
+  }
+
+  private static void processLocalInformation() {
+    String separator = FileSystems.getDefault().getSeparator();
+    cdw = Paths.get("").toAbsolutePath().toString();
+    localJava = System.getProperty("java.home") + separator + "bin" + separator + "java";
+    localJavaVersion = getLocalJavaVersion();
+  }
+
+  public static void displayLocalInformation() {
+    System.out.println("Current working directory: " + cdw);
+    System.out.println("Path to local Java: " + localJava);
+    System.out.println("Local Java version: " + localJavaVersion);
   }
 
   public static void startSecondJVM() throws Exception {
@@ -34,32 +58,12 @@ public class Multitool {
     Runtime.getRuntime().exec(command);
   }
 
-  private static void processMultitool() {
-
-    List<String> tools = processDirectory("multitool");
-    System.out.println("Local multitool jar files.");
-    System.out.println(tools);
-    String latest = getMultitoolRelease();
-    System.out.println("Latest multitool release: " + latest);
-    System.out.println("End multitool");
-  }
-
-  private static void processKolMafia() {
-    List<String> tools = processDirectory("KoLmafia");
-    System.out.println("Local KoLmafia jar files.");
-    System.out.println(tools);
-    String latest = getMafiaRelease();
-    System.out.println("latest KoLmafia release: " + latest);
-    System.out.println("End KoLmafia");
-  }
-
   private static List<String> processDirectory(String nameRoot) {
     // Returns a list of file names in the current directory that match
     List<String> retVal = new ArrayList<>();
     String lcRoot = nameRoot.toLowerCase();
-    String currentWorkingDir = Paths.get("").toAbsolutePath().toString();
     try {
-      File f = new File(currentWorkingDir);
+      File f = new File(cdw);
       String[] files = f.list();
       if (files != null) {
         for (String file : files) {
@@ -75,61 +79,68 @@ public class Multitool {
     return retVal;
   }
 
-  private static String getMafiaRelease() {
-    String rel = "https://api.github.com/repos/kolmafia/kolmafia/releases/latest";
-    String retVal;
-    URL url;
-    try {
-      url = new URL(rel);
-    } catch (MalformedURLException e) {
-      throw new RuntimeException(e);
-    }
-    InputStream is;
-    try {
-      is = url.openStream();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    int ptr;
-    StringBuilder buffer = new StringBuilder();
-    while (true) {
-      try {
-        if ((ptr = is.read()) == -1) break;
-      } catch (IOException e) {
-        throw new RuntimeException(e);
+  private static int getLocalJavaVersion() {
+    new StringBuilder("Unknown");
+    StringBuilder local;
+    char[] pp = System.getProperty("java.home").toCharArray();
+    int end = pp.length - 1;
+    boolean first;
+    first = false;
+    local = new StringBuilder();
+    for (int x = end; x >= 0; x--) {
+      char ppp = pp[x];
+      if (Character.isDigit(ppp)) {
+        first = true;
+        local.insert(0, ppp);
+      } else {
+        if (first) break;
       }
-      buffer.append((char) ptr);
     }
-    String dq = "\"";
-    String js = buffer.toString();
-    String findMe = dq + "name" + dq + ":";
-    int i = js.indexOf(findMe);
-    js = js.substring(i + findMe.length());
-    i = js.indexOf(",");
-    js = js.substring(0, i);
-    js = js.replaceAll("\"", "");
-    retVal = js;
+    return Integer.parseInt(local.toString());
+  }
+
+  private static ToolData processTool(String toolName) {
+    ToolData retVal = new ToolData(toolName);
+    retVal.setLatestVersion(getLatestReleaseVersion(toolName));
+    retVal.setLocalJars(processDirectory(toolName));
+    retVal.setLocalModificationFound(false);
+    List<String> locals = retVal.getLocalJars();
+    int localVersion = 0;
+    for (String jarName : locals) {
+      int i = jarName.indexOf(toolName);
+      String hold = jarName.substring(i + toolName.length() + 1);
+      i = hold.indexOf(".jar");
+      hold = hold.substring(0, i);
+      if (hold.contains("-M")) {
+        i = hold.indexOf("-M");
+        hold = hold.substring(0, i);
+        retVal.setLocalModificationFound(true);
+      }
+      localVersion = Integer.parseInt(hold);
+    }
+    retVal.setCurrentVersion(localVersion);
     return retVal;
   }
 
-  private static String getMultitoolRelease() {
+  private static void displayToolInformation(ToolData tool) {
+    System.out.println(tool);
+  }
 
-    String rel = "https://api.github.com/repos/kolmafia/multitool/releases/latest";
+  private static int getLatestReleaseVersion(String tool) {
+    String rel = "https://api.github.com/repos/kolmafia/" + tool + "/releases/latest";
     String retVal;
     URL url;
     try {
       url = new URL(rel);
     } catch (MalformedURLException e) {
-      System.out.println(e);
-      retVal = "Unknown";
-      return retVal;
+      throw new RuntimeException(e);
     }
     InputStream is;
     try {
       is = url.openStream();
     } catch (IOException e) {
-      retVal = "Unknown";
-      return retVal;
+      System.out.println(e);
+      return 0;
     }
     int ptr;
     StringBuilder buffer = new StringBuilder();
@@ -138,8 +149,7 @@ public class Multitool {
         if ((ptr = is.read()) == -1) break;
       } catch (IOException e) {
         System.out.println(e);
-        retVal = "Unknown";
-        return retVal;
+        return 0;
       }
       buffer.append((char) ptr);
     }
@@ -152,31 +162,13 @@ public class Multitool {
     js = js.substring(0, i);
     js = js.replaceAll("\"", "");
     retVal = js;
-    return retVal;
+    return Integer.parseInt(retVal);
   }
 
-  private static void processJava() {
-    String javaVersion = getJavaVersion();
-    System.out.println("Local, running Java version: " + javaVersion);
-  }
-
-  private static String getJavaVersion() {
-    new StringBuilder("Unknown");
-    StringBuilder retVal;
-    char[] pp = System.getProperty("java.home").toCharArray();
-    int end = pp.length - 1;
-    boolean first;
-    first = false;
-    retVal = new StringBuilder();
-    for (int x = end; x >= 0; x--) {
-      char ppp = pp[x];
-      if (Character.isDigit(ppp)) {
-        first = true;
-        retVal.insert(0, ppp);
-      } else {
-        if (first) break;
-      }
-    }
-    return retVal.toString();
+  private static int getPreferredJava() {
+    int version = 21;
+    // I can't actually figure out where to get this from GitHub or KoLmafia.
+    // Deferred for the moment.
+    return version;
   }
 }
---
+++ src/com/github/multitool/ToolData.java
@@ -0,0 +1,84 @@
+package com.github.multitool;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToolData {
+  private final String toolName;
+  private int currentVersion;
+  private int latestVersion;
+  private Path downloadURL;
+  private List<String> localJars = new ArrayList<>();
+  private boolean localModificationFound;
+
+  public ToolData(String name) {
+    this.toolName = name;
+  }
+
+  public int getLatestVersion() {
+    return latestVersion;
+  }
+
+  public void setLatestVersion(int latestVersion) {
+    this.latestVersion = latestVersion;
+  }
+
+  public int getCurrentVersion() {
+    return currentVersion;
+  }
+
+  public void setCurrentVersion(int currentVersion) {
+    this.currentVersion = currentVersion;
+  }
+
+  public List<String> getLocalJars() {
+    return localJars;
+  }
+
+  public void setLocalJars(List<String> localJars) {
+    this.localJars = localJars;
+  }
+
+  public String getToolName() {
+    return toolName;
+  }
+
+  public Path getDownloadURL() {
+    return downloadURL;
+  }
+
+  public void setDownloadURL(Path downloadURL) {
+    this.downloadURL = downloadURL;
+  }
+
+  @Override
+  public String toString() {
+    String NL = "\n";
+    return "ToolData for "
+        + toolName
+        + NL
+        + "currentVersion="
+        + currentVersion
+        + NL
+        + "latestVersion="
+        + latestVersion
+        + NL
+        + "downloadURL="
+        + downloadURL
+        + NL
+        + "localJars="
+        + localJars
+        + NL
+        + "localModificationFound="
+        + localModificationFound;
+  }
+
+  public boolean isLocalModificationFound() {
+    return localModificationFound;
+  }
+
+  public void setLocalModificationFound(boolean localModificationFound) {
+    this.localModificationFound = localModificationFound;
+  }
+}
