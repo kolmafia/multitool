3c80f0613a3534cda17f19d5226db1fba2b74f30
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2025-03-15 14:28:27 -0400

    Refactored ToolData

--- src/us/kolmafia/multitool/ToolData.java
+++ src/us/kolmafia/multitool/ToolData.java
@@ -5,104 +5,104 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ToolData {
-  private final String toolName;
-  private int currentVersion;
-  private int latestVersion;
-  private String downloadURL;
-  private File latestJarFile;
-  private List<String> localJars = new ArrayList<>();
-  private boolean localModificationFound;
-  private boolean needToDownload;
+    private final String toolName;
+    private int currentVersion;
+    private int latestVersion;
+    private String downloadURL;
+    private File latestJarFile;
+    private List<String> localJars = new ArrayList<>();
+    private boolean localModificationFound;
+    private boolean needToDownload;
 
-  public ToolData(String name) {
-    this.toolName = name;
-  }
+    public ToolData(String name) {
+        this.toolName = name;
+    }
 
-  public int getLatestVersion() {
-    return latestVersion;
-  }
+    public int getLatestVersion() {
+        return latestVersion;
+    }
 
-  public void setLatestVersion(int latestVersion) {
-    this.latestVersion = latestVersion;
-  }
+    public void setLatestVersion(int latestVersion) {
+        this.latestVersion = latestVersion;
+    }
 
-  public int getCurrentVersion() {
-    return currentVersion;
-  }
+    public int getCurrentVersion() {
+        return currentVersion;
+    }
 
-  public void setCurrentVersion(int currentVersion) {
-    this.currentVersion = currentVersion;
-  }
+    public void setCurrentVersion(int currentVersion) {
+        this.currentVersion = currentVersion;
+    }
 
-  public List<String> getLocalJars() {
-    return localJars;
-  }
+    public List<String> getLocalJars() {
+        return localJars;
+    }
 
-  public void setLocalJars(List<String> localJars) {
-    this.localJars = localJars;
-  }
+    public void setLocalJars(List<String> localJars) {
+        this.localJars = localJars;
+    }
 
-  public String getToolName() {
-    return toolName;
-  }
+    public String getToolName() {
+        return toolName;
+    }
 
-  public String getDownloadURL() {
-    return downloadURL;
-  }
+    public String getDownloadURL() {
+        return downloadURL;
+    }
 
-  public void setDownloadURL(String downloadURL) {
-    this.downloadURL = downloadURL;
-  }
+    public void setDownloadURL(String downloadURL) {
+        this.downloadURL = downloadURL;
+    }
 
-  @Override
-  public String toString() {
-    String NL = "\n";
-    return "ToolData for "
-        + toolName
-        + NL
-        + "currentVersion="
-        + currentVersion
-        + NL
-        + "latestVersion="
-        + latestVersion
-        + NL
-        + "downloadURL="
-        + downloadURL
-        + NL
-        + "needToDownload="
-        + needToDownload
-        + NL
-        + "localJars="
-        + localJars
-        + NL
-        + "latestJarFile="
-        + latestJarFile
-        + NL
-        + "localModificationFound="
-        + localModificationFound;
-  }
+    @Override
+    public String toString() {
+        String NL = "\n";
+        return "ToolData for "
+                + toolName
+                + NL
+                + "currentVersion="
+                + currentVersion
+                + NL
+                + "latestVersion="
+                + latestVersion
+                + NL
+                + "downloadURL="
+                + downloadURL
+                + NL
+                + "needToDownload="
+                + needToDownload
+                + NL
+                + "localJars="
+                + localJars
+                + NL
+                + "latestJarFile="
+                + latestJarFile
+                + NL
+                + "localModificationFound="
+                + localModificationFound;
+    }
 
-  public boolean isLocalModificationFound() {
-    return localModificationFound;
-  }
+    public boolean isLocalModificationFound() {
+        return localModificationFound;
+    }
 
-  public void setLocalModificationFound(boolean localModificationFound) {
-    this.localModificationFound = localModificationFound;
-  }
+    public void setLocalModificationFound(boolean localModificationFound) {
+        this.localModificationFound = localModificationFound;
+    }
 
-  public File getLatestJarFile() {
-    return latestJarFile;
-  }
+    public File getLatestJarFile() {
+        return latestJarFile;
+    }
 
-  public void setLatestJarFile(File latestJar) {
-    this.latestJarFile = latestJar;
-  }
+    public void setLatestJarFile(File latestJar) {
+        this.latestJarFile = latestJar;
+    }
 
-  public boolean isNeedToDownload() {
-    return needToDownload;
-  }
+    public boolean isNeedToDownload() {
+        return needToDownload;
+    }
 
-  public void setNeedToDownload(boolean needToDownload) {
-    this.needToDownload = needToDownload;
-  }
+    public void setNeedToDownload(boolean needToDownload) {
+        this.needToDownload = needToDownload;
+    }
 }
