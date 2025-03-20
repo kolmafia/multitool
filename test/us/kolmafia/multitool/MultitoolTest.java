cd76df4b7d33c28369e892924d8803885dbdbc12
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2025-03-20 11:31:11 -0400

    Park

--- test/us/kolmafia/multitool/MultitoolTest.java
+++ test/us/kolmafia/multitool/MultitoolTest.java
@@ -5,7 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static us.kolmafia.multitool.Multitool.*;
+import static us.kolmafia.multitool.Multitool.ROOT_LOCATION;
+import static us.kolmafia.multitool.Multitool.cleanPath;
+import static us.kolmafia.multitool.Multitool.cwd;
+import static us.kolmafia.multitool.Multitool.processDirectory;
+import static us.kolmafia.multitool.Multitool.processLocalInformation;
 
 import java.io.File;
 import java.io.IOException;
